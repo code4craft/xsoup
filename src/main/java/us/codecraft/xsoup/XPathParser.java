a35974522acc946d4f7b09647043bc97ab5859f1
yihua.huang <code4crafter@gmail.com>
2014-03-04 13:58:51 +0800

    refactor byAttribute to stateless

--- src/main/java/us/codecraft/xsoup/XPathParser.java
+++ src/main/java/us/codecraft/xsoup/XPathParser.java
@@ -99,18 +99,25 @@ public class XPathParser {
         } else if (tq.matches("*")) {
             allElements();
         } else if (tq.matchesRegex("\\w+\\(.*\\)")) {
-            consumeFunction();
+            consumeOperatorFunction();
         } else if (tq.matchesWord()) {
             byTag();
         } else if (tq.matches("[@")) {
-            byAttribute();
+            evals.add(byAttribute(tq.chompBalanced('[', ']')));
         } else if (tq.matchesRegex("\\[\\d+\\]")) {
             byNth();
+        } else if (tq.matchesRegex("\\[\\w+\\(.*\\)\\]")) {
+            consumeEvaluatorFunction();
         } else {
             // unhandled
             throw new Selector.SelectorParseException("Could not parse query '%s': unexpected token at '%s'", query, tq.remainder());
         }
 
+    }
+
+    private Evaluator consumeEvaluatorFunction() {
+        XTokenQueue functionQueue = new XTokenQueue(tq.chompBalanced('[', ']'));
+        return null;
     }
 
     private void allElements() {
@@ -131,45 +138,43 @@ public class XPathParser {
 
     private Pattern patternForText = Pattern.compile("text\\((\\d*)\\)");
 
-    private void consumeFunction() {
+    private void consumeOperatorFunction() {
         String remainder = tq.remainder();
-        functionText(remainder);
-        if (elementOperator == null) {
+        if (remainder.startsWith("text(")) {
+            functionText(remainder);
+        } else if (remainder.startsWith("regex(")) {
             functionRegex(remainder);
+        } else if (remainder.equals("allText()")) {
+            elementOperator = new ElementOperator.AllText();
+        } else if (remainder.equals("tidyText()")) {
+            elementOperator = new ElementOperator.TidyText();
+        } else if (remainder.equals("html()")) {
+            elementOperator = new ElementOperator.Html();
+        } else if (remainder.equals("outerHtml()")) {
+            elementOperator = new ElementOperator.OuterHtml();
+        } else {
+            throw new IllegalArgumentException("Unsupported function " + remainder);
         }
-        if (elementOperator == null) {
-            if (remainder.equals("allText()") || remainder.equals("text()")) {
-                elementOperator = new ElementOperator.AllText();
-            } else if (remainder.equals("tidyText()")) {
-                elementOperator = new ElementOperator.TidyText();
-            } else if (remainder.equals("html()")) {
-                elementOperator = new ElementOperator.Html();
-            } else if (remainder.equals("outerHtml()")) {
-                elementOperator = new ElementOperator.OuterHtml();
-            } else {
-                throw new IllegalArgumentException("Unsupported function " + remainder);
-            }
+        if (elementOperator != null) {
+            noEvalAllow = true;
         }
-        noEvalAllow = true;
     }
 
     private void functionRegex(String remainder) {
-        if (remainder.startsWith("regex(")) {
-            Validate.isTrue(remainder.endsWith(")"), "Unclosed bracket for function! " + remainder);
-            List<String> params = XTokenQueue.trimQuotes(XTokenQueue.parseFuncionParams(remainder.substring("regex(".length(), remainder.length() - 1)));
-            if (params.size() == 1) {
-                elementOperator = new ElementOperator.Regex(params.get(0));
-            } else if (params.size() == 2) {
-                if (params.get(0).startsWith("@")) {
-                    elementOperator = new ElementOperator.Regex(params.get(1), params.get(0).substring(1));
-                } else {
-                    elementOperator = new ElementOperator.Regex(params.get(0), null, Integer.parseInt(params.get(1)));
-                }
-            } else if (params.size() == 3) {
-                elementOperator = new ElementOperator.Regex(params.get(1), params.get(0).substring(1), Integer.parseInt(params.get(2)));
+        Validate.isTrue(remainder.endsWith(")"), "Unclosed bracket for function! " + remainder);
+        List<String> params = XTokenQueue.trimQuotes(XTokenQueue.parseFuncionParams(remainder.substring("regex(".length(), remainder.length() - 1)));
+        if (params.size() == 1) {
+            elementOperator = new ElementOperator.Regex(params.get(0));
+        } else if (params.size() == 2) {
+            if (params.get(0).startsWith("@")) {
+                elementOperator = new ElementOperator.Regex(params.get(1), params.get(0).substring(1));
             } else {
-                throw new Selector.SelectorParseException("Unknown usage for regex()" + remainder);
+                elementOperator = new ElementOperator.Regex(params.get(0), null, Integer.parseInt(params.get(1)));
             }
+        } else if (params.size() == 3) {
+            elementOperator = new ElementOperator.Regex(params.get(1), params.get(0).substring(1), Integer.parseInt(params.get(2)));
+        } else {
+            throw new Selector.SelectorParseException("Unknown usage for regex()" + remainder);
         }
     }
 
@@ -198,18 +203,18 @@ public class XPathParser {
         evals.add(new Evaluator.Tag(tagName.trim().toLowerCase()));
     }
 
-    private void byAttribute() {
-        XTokenQueue cq = new XTokenQueue(tq.chompBalanced('[', ']')); // content queue
+    private Evaluator byAttribute(String exp) {
+        XTokenQueue cq = new XTokenQueue(exp); // content queue
         cq.matchChomp("@");
         String key = cq.consumeToAny("=", "!=", "^=", "$=", "*=", "~="); // eq, not, start, end, contain, match, (no val)
         Validate.notEmpty(key);
         cq.consumeWhitespace();
-
+        Evaluator evaluator;
         if (cq.isEmpty()) {
             if ("*".equals(key)) {
-                evals.add(new XEvaluators.HasAnyAttribute());
+                evaluator = new XEvaluators.HasAnyAttribute();
             } else {
-                evals.add(new Evaluator.Attribute(key));
+                evaluator = new Evaluator.Attribute(key);
             }
         } else {
             if (cq.matchChomp("=")) {
@@ -217,30 +222,31 @@ public class XPathParser {
                 if (key.equals("class")) {
                     String className = XTokenQueue.trimQuotes(cq.remainder());
                     if (!className.contains(" ")) {
-                        evals.add(new Evaluator.Class(className));
+                        evaluator = new Evaluator.Class(className);
                     } else {
-                        evals.add(new Evaluator.AttributeWithValue(key, className));
+                        evaluator = new Evaluator.AttributeWithValue(key, className);
                     }
                 } else {
-                    evals.add(new Evaluator.AttributeWithValue(key, XTokenQueue.trimQuotes(cq.remainder())));
+                    evaluator = new Evaluator.AttributeWithValue(key, XTokenQueue.trimQuotes(cq.remainder()));
                 }
             } else if (cq.matchChomp("!="))
-                evals.add(new Evaluator.AttributeWithValueNot(key, XTokenQueue.trimQuotes(cq.remainder())));
+                evaluator = new Evaluator.AttributeWithValueNot(key, XTokenQueue.trimQuotes(cq.remainder()));
 
             else if (cq.matchChomp("^="))
-                evals.add(new Evaluator.AttributeWithValueStarting(key, XTokenQueue.trimQuotes(cq.remainder())));
+                evaluator = new Evaluator.AttributeWithValueStarting(key, XTokenQueue.trimQuotes(cq.remainder()));
 
             else if (cq.matchChomp("$="))
-                evals.add(new Evaluator.AttributeWithValueEnding(key, XTokenQueue.trimQuotes(cq.remainder())));
+                evaluator = new Evaluator.AttributeWithValueEnding(key, XTokenQueue.trimQuotes(cq.remainder()));
 
             else if (cq.matchChomp("*="))
-                evals.add(new Evaluator.AttributeWithValueContaining(key, XTokenQueue.trimQuotes(cq.remainder())));
+                evaluator = new Evaluator.AttributeWithValueContaining(key, XTokenQueue.trimQuotes(cq.remainder()));
 
             else if (cq.matchChomp("~="))
-                evals.add(new Evaluator.AttributeWithValueMatching(key, Pattern.compile(XTokenQueue.trimQuotes(cq.remainder()))));
+                evaluator = new Evaluator.AttributeWithValueMatching(key, Pattern.compile(XTokenQueue.trimQuotes(cq.remainder())));
             else
                 throw new Selector.SelectorParseException("Could not parse attribute query '%s': unexpected token at '%s'", query, cq.remainder());
         }
+        return evaluator;
     }
 
     public static XPathEvaluator parse(String xpathStr) {
